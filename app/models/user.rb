db8080f2f3edc330f8793544690dddad6fa142e1
hidemaru11 <do.first.jp@gmail.com>
2020-10-02 07:33:03 +0900

    Change user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
-  
-  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/
-  validates :email, presence: true,
-            uniqueness: true,
-            format: { with: VALID_EMAIL_REGEX }
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
-  validates :password, presence: true,
-            confirmation: true,
-            format: { with: VALID_PASSWORD_REGEX,
-            message: "は半角英字と半角数字をどちらも含み、8文字以上32文字以下で入力してください。" }
+         :recoverable, :rememberable, :validatable
   validates :username, presence: true
 end
