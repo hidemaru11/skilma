160db0baa3e031df3e082eb06385edc9dc69773c
hidemaru11 <do.first.jp@gmail.com>
2020-10-10 09:46:27 +0900

    Add config time zone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Skilma
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       %Q(#{html_tag}).html_safe
     end
+
+    config.time_zone = 'Asia/Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
