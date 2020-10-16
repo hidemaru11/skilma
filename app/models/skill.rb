03e63cda2938eab63ca6c647813e431f20880a9d
hidemaru11 <do.first.jp@gmail.com>
2020-10-17 07:44:02 +0900

    Add skill new form

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,7 @@ class Skill < ApplicationRecord
   belongs_to :user
 
   validates :title, length: { in: 3..100 }
-  validates :content, length: { in: 3..1000 }
+  validates :content, length: { in: 3..2000 }
 
   scope :sorted_desc, -> { order(created_at: :desc) }
 end
