5a91e17f77207782f45d7c8f956586619ece73ff
hidemaru11 <do.first.jp@gmail.com>
2020-10-19 07:51:23 +0900

    Add plan model

---
+++ app/models/plan.rb
@@ -0,0 +1,8 @@
+class Plan < ApplicationRecord
+  belongs_to :skill
+  belongs_to :budget_unit
+
+  validates :title, length: { in: 3..100 }
+  validates :content, length: { in: 3..1000 }
+  validates :budget, numericality: { greater_tha_or_equal_to: 500, less_than_or_equal_to: 300000 }
+end
