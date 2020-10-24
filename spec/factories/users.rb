30cae430f7864f16fdf2e5d59d778694f3a08543
hidemaru11 <do.first.jp@gmail.com>
2020-10-25 06:34:36 +0900

    Edit user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     email { "steven@example.com" }
     username { "steven" }
     password { "pass1234" }
+    password_confirmation { "pass1234" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,57 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    expect(FactoryBot.build(:user)).to be_valid
+  describe '#create' do
+
+    context 'can save' do
+      it "has a valid factory" do
+        expect(FactoryBot.build(:user)).to be_valid
+      end
+    end
+
+    context "can not save" do
+      it "is invalid without an email" do
+        user = FactoryBot.build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+
+      it "is invalid with a duplicate email" do
+        FactoryBot.create(:user, email: "steven@example.com")
+        user = FactoryBot.build(:user, email: "steven@example.com")
+        user.valid?
+        expect(user.errors[:email]).to include("はすでに存在します")
+      end
+
+      it "is invalid without a username" do
+        user = FactoryBot.build(:user, username: nil)
+        user.valid?
+        expect(user.errors[:username]).to include("を入力してください")
+      end
+
+      it "is invalid without a password" do
+        user = FactoryBot.build(:user, password: nil)
+        user.valid?
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+      
+      it "is invalid if length is less than 7" do
+        user = FactoryBot.build(:user, password: "a" * 7)
+        user.valid?
+        expect(user.errors[:password]).to include("は8文字以上で入力してください")
+      end
+
+      it "is invalid if length is more than 33" do
+        user = FactoryBot.build(:user, password: "a" * 33)
+        user.valid?
+        expect(user.errors[:password]).to include("は32文字以内で入力してください")
+      end
+
+      it "is invalid without a password confirmation although with a password" do
+        user = FactoryBot.build(:user, password_confirmation: "")
+        user.valid?
+        expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+      end
+    end
+    
   end
-
-  describe 'email' do
-    it "is invalid without an email" do
-      user = FactoryBot.build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("を入力してください")
-    end
-
-    it "is invalid with a duplicate email" do
-      FactoryBot.create(:user, email: "steven@example.com")
-      user = FactoryBot.build(:user, email: "steven@example.com")
-      user.valid?
-      expect(user.errors[:email]).to include("はすでに存在します")
-    end
-  end
-
-  describe 'username' do
-    it "is invalid without a username" do
-      user = FactoryBot.build(:user, username: nil)
-      user.valid?
-      expect(user.errors[:username]).to include("を入力してください")
-    end
-  end
-
-  describe 'password' do
-    it "is invalid without a password" do
-      user = FactoryBot.build(:user, password: nil)
-      user.valid?
-      expect(user.errors[:password]).to include("を入力してください")
-    end
-
-    it "is invalid if length is less than 7" do
-      user = FactoryBot.build(:user, password: "a" * 7)
-      user.valid?
-      expect(user.errors[:password]).to include("は8文字以上で入力してください")
-    end
-
-    it "is invalid if length is more than 33" do
-      user = FactoryBot.build(:user, password: "a" * 33)
-      user.valid?
-      expect(user.errors[:password]).to include("は32文字以内で入力してください")
-    end
-
-    it "is valid if length is 8 to 32" do
-      user = FactoryBot.build(:user, password: "1a" * 4)
-      expect(user).to be_valid
-    end
-  end
-
-
 end
