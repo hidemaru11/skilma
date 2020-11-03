8124b3948f41b229b2ef6b9b72a47c1fce0cb8ac
hidemaru11 <do.first.jp@gmail.com>
2020-11-04 07:34:26 +0900

    Add basic message form

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,8 +3,8 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
-    @entry2 = Entry.create(entry_params).merge(room_id: @room.id))
-    redirect_to "/rooms/#{@room.id}"
+    @entry2 = Entry.create((entry_params).merge(room_id: @room.id))
+    redirect_to @room
   end
 
   def show
