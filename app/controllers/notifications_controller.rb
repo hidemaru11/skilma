b1d5cf10128cd92620b2d9b7698cabeb408af71f
hidemaru11 <do.first.jp@gmail.com>
2020-11-15 08:31:36 +0900

    Add notification icon

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.sorted_desc
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
