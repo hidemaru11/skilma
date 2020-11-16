67cb0baa753819d8351077124cc7935664148de1
hidemaru11 <do.first.jp@gmail.com>
2020-11-17 07:50:39 +0900

    Add guest login

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_out', to: 'users/sessions#destroy'
+    post 'guest_sign_in', to: 'users/sessions#new_guest'
   end
   
   root 'home#index'
