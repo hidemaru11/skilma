c34c86573e081c321ee012d1249f3454e99f009a
hidemaru11 <do.first.jp@gmail.com>
2020-12-07 06:38:32 +0900

    Change shrine config production strages to s3

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,22 @@
 require "shrine"
 require "shrine/storage/file_system"
+require 'shrine/storage/s3'
 
-Shrine.storages = {
+if Rails.env.production?
+  s3_options = {
+    access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    region:            "ap-northeast-1",
+    bucket:            "skilmaimage"}
+
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
+else
+  Shrine.storages = {
     cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
     store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")}
+end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
